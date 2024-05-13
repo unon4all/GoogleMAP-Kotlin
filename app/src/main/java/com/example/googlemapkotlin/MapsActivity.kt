0280fe06eb987ef8869e49b8ac81b464db72a1ec
unon <unon4all@gmail.com>
2024-05-13 22:48:34 +0530

    version - 1.0.6
    - Polyline

--- app/src/main/java/com/example/googlemapkotlin/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapkotlin/MapsActivity.kt
@@ -1,29 +1,37 @@
 package com.example.googlemapkotlin
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.googlemapkotlin.databinding.ActivityMapsBinding
 import com.example.googlemapkotlin.misc.CameraAndViewPort
-import com.example.googlemapkotlin.misc.CustomInfoAdapter
 import com.example.googlemapkotlin.misc.TypeAndStyle
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
+import com.google.android.gms.maps.GoogleMap.OnPolylineClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnPolylineClickListener {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val pune = LatLng(18.5204, 73.8567)
+    private val mumbai = LatLng(19.0760, 72.8777)
+    private val delhi = LatLng(28.7041, 77.1025)
+    private val agra = LatLng(27.1766, 78.0422)
+    private val chennai = LatLng(13.0827, 80.2707)
 
     private val typeAndStyle by lazy { TypeAndStyle() }
 
@@ -56,21 +64,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListe
         map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val pune = LatLng(18.5204, 73.8567)
-        val pune2 = LatLng(18.457872, 73.857959)
-        val newYork = LatLng(40.7128, -74.0060)
+
 
         val puneMarker = map.addMarker(
             MarkerOptions().position(pune).title("Marker in Pune")
                 .snippet("This is a Marker in Pune")
         )
 
-        val puneMarker2 = map.addMarker(
-            MarkerOptions().position(pune2).title("Marker in Pune2").zIndex(1f)
-                .snippet("This is a Marker in Pune2")
-        )
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pune, 10f))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pune, 5f))
 
         //UI Settings
         map.uiSettings.apply {
@@ -81,22 +83,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListe
         typeAndStyle.setMapStyle(googleMap = googleMap, context = this)
 
         lifecycleScope.launch {
-            delay(4000L)
-
+            addPolyLine()
         }
+    }
 
-        map.setOnMarkerClickListener(this)
 
-        map.setInfoWindowAdapter(CustomInfoAdapter(this))
+    private suspend fun addPolyLine() {
+
+        val polyLine = map.addPolyline(PolylineOptions().apply {
+            add(pune, mumbai)
+            width(10f)
+            color(Color.BLUE)
+            geodesic(true)
+            clickable(true)
+        })
+
+        delay(5000)
+
+        val newList = listOf(pune, mumbai, delhi, agra, chennai)
+
+        polyLine.points = newList
 
     }
 
-    override fun onMarkerClick(marker: Marker): Boolean {
-        map.animateCamera(CameraUpdateFactory.zoomTo(10f), 2000, null)
-        marker.showInfoWindow()
-        return true
+    override fun onPolylineClick(p0: Polyline) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
     }
-
 
 }
 
